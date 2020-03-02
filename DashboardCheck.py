c0a4ad28e3bf978fa65f590b295312a4358b2ebf
dwindageekseat <dwinda.yudantoro@geekseat.com.au>
2020-03-02 14:02:40 +0800

    Adding master script

---
+++ DashboardCheck.py
@@ -0,0 +1,63 @@
+# -*- encoding=utf8 -*-
+__author__ = "Dwinda Yudantoro"
+
+from airtest.core.api import *
+import os
+import sys
+
+auto_setup(__file__)
+
+regulartime = 2
+longtime = 5
+network = "com.android.WifiState
+network2 = "com.android.network"
+
+def main():
+    
+    loginSite()
+    if len(sys.argv) <> 3:
+        sys.stderr.write('no network detected')
+        sys.exit(2)
+    network = sys.argv[1], sys.argv[2]
+    try:
+        stat1 = os.stat(network1)
+    except os.error:
+        sys.stderr.write( network2 + ': cannot start without connection')
+        sys.exit(1)
+    try:
+        os.utime(file2, (stat1[ST_ATIME], stat1[ST_MTIME]))
+    except os.error:
+        sys.stderr.write(file2 + ': cannot change time\n')
+        sys.exit(2)
+
+if __name__ == '__main__':
+opportunityCheck()
+    try:
+        stat1=os.stat(network1)
+    cat
+
+def loginSite():
+    
+    touch(Template(r"tpl1582075140767.png", record_pos=(-0.394, -0.694), resolution=(1080, 2340)))
+
+    text("gama@gmail.com")
+    touch(Template(r"tpl1582075015092.png", record_pos=(-0.009, -0.293), resolution=(1080, 2220)))
+    sleep(regulartime)
+    touch(Template(r"tpl1582075163904.png", record_pos=(-0.356, -0.673), resolution=(1080, 2340)))
+    text("123")
+    touch(Template(r"tpl1582075299305.png", record_pos=(0.001, -0.38), resolution=(1080, 2340)))
+
+def opportunityCheck():
+    touch(Template(r"tpl1582076014102.png", record_pos=(-0.206, -0.214), resolution=(1080, 2340)))
+    sleep(2)
+    touch(Template(r"tpl1582076175577.png", record_pos=(-0.206, -0.746), resolution=(1080, 2340)))
+    text("jojon markijon")
+    touch(Template(r"tpl1582076253912.png", record_pos=(0.283, -0.561), resolution=(1080, 2340)))
+    sleep(1)
+    touch(Template(r"tpl1582076517959.png", record_pos=(-0.278, -0.745), resolution=(1080, 2340)))
+    text("jojon naek motor")
+    
+    
+main()
+loginSite()
+opportunityCheck()  
---
+++ PitchFlick.py
@@ -0,0 +1,74 @@
+# -*- encoding=utf8 -*-
+__author__ = "Dwinda Yudantoro"
+__title__ = "Quick Check"
+__desc__= """ Quick Check on Dashboard """
+
+from airtest.core.api import *
+auto_setup(__file__)
+
+#=============additional variable start here===================
+sleepTime = 2
+#==============================================================
+
+#
+# Testcase simple aja lah ya, ngapain dibuat ribet-ribet ?
+#
+
+#==============================================================
+# Instagram test
+#note : Coba ngetest
+print('launch the AUT, please wait . . .')
+sleep(sleepTime)
+wake()
+sleep(sleepTime)
+start_app ('com.android.PitchFlick');
+
+#==============================================================
+# test case flow related gameplay test with skip tutorial
+def AboutPhone ():
+    print('Opening apps done')
+sleep(sleepTime)
+print('Tap on About button')
+while (exists(Template(r"tpl1579673051655.png", record_pos=(0.019, -0.916), resolution=(1080, 2340)))==0):
+    print('Entering Setting')
+    
+if exists(Template(r"tpl1579672947168.png", record_pos=(-0.006, -0.637), resolution=(1080, 2340))) :
+    touch(Template(r"tpl1579672960165.png", record_pos=(0.002, -0.619), resolution=(1080, 2340)))
+    sleep(sleepTime)
+    print('Tapping About button')
+    #touch()
+if exists(Template(r"tpl1579673116021.png", record_pos=(0.01, -0.909), resolution=(1080, 2340))):
+    sleep(sleepTime)
+    touch(Template(r"tpl1579673128734.png", record_pos=(-0.272, -0.772), resolution=(1080, 2340)))
+
+    print('Tapping System update')
+    sleep(sleepTime)
+while (exists(Template(r"tpl1579674045430.png", record_pos=(-0.003, 0.948), resolution=(1080, 2340)))!=0):
+    print('Touch Check for updates')
+    touch(Template(r"tpl1579674192790.png", record_pos=(-0.416, -0.952), resolution=(1080, 2340)))
+    sleep(sleepTime)
+    
+    touch(Template(r"tpl1579673207701.png", record_pos=(-0.41, -0.921), resolution=(1080, 2340)))
+        
+def checkDisplay():
+    print("Entering Display Check")
+    while (exists(Template(r"tpl1579679758459.png", record_pos=(0.012, -0.922), resolution=(1080, 2340)))!=0):
+        touch(Template(r"tpl1579680089472.png", record_pos=(-0.277, 0.919), resolution=(1080, 2340)))
+        
+        print("Entering Display")
+        sleep(sleepTime)
+        
+        touch(Template(r"tpl1579680184896.png", record_pos=(-0.265, -0.683), resolution=(1080, 2340)))
+        sleep(sleepTime)
+        swipe(Template(r"tpl1579680212788.png", record_pos=(0.019, -0.58), resolution=(1080, 2340)), vector=[0.4408, 0.0197])
+        sleep(sleepTime)
+        touch(Template(r"tpl1579680256160.png", record_pos=(-0.406, -0.924), resolution=(1080, 2340)))
+        type("test airtest")
+        
+def checkSecurity():
+    print("Checking security screen")
+    touch(tpl1579679758459)
+#===========================
+AboutPhone()
+wifiCheck()
+checkDisplay()
